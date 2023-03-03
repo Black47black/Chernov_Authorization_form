53de1890fd17db6ff1b9331465d943b5b9d7f72a
Dmitry Black <black47black@gmail.com>
2023-03-03 13:51:23 +0400

    add date

--- script.js
+++ script.js
@@ -29,27 +29,70 @@ form.addEventListener('submit', function (e) {
     return;
   }
 
+  if (password)
+
   if (password.lenght < 6) {
-    showError('Невалидный пароль');
+    showError('Пароль меньше 6 символов');
     return;
   }
 
-  if (login !== "piu@piu.piu") {
-    showError('Логин не найден');
-    return;
-  }
+  // if (login !== "piu@piu.piu") {
+  //   showError('Логин не найден');
+  //   return;
+  // }
 
-  if (password !== "1234567890") {
-    showError('Не правильный пароль');
-    return;
-  }
+  // if (password !== "1234567890") {
+  //   showError('Не правильный пароль');
+  //   return;
+  // }
 
   hideError();
   alert('Добро пожаловть!');
 }); 
 
+// setInterval(function () {
+//   var now = new Date();
+//   var clock = document.getElementById("clock");
+//   clock.innerHTML = now.toLocaleTimeString();
+// }, 1000);
+
+function zeroFirstFormat(value)
+{
+    if (value < 10)
+    {
+        value='0'+value;
+    }
+    return value;
+}
+
+/* функция получения текущей даты и времени */
+function date_time()
+{
+    var current_datetime = new Date();
+    var day = zeroFirstFormat(current_datetime.getDate());
+    var month = zeroFirstFormat(current_datetime.getMonth()+1);
+    var year = current_datetime.getFullYear();
+    var hours = zeroFirstFormat(current_datetime.getHours());
+    var minutes = zeroFirstFormat(current_datetime.getMinutes());
+    var seconds = zeroFirstFormat(current_datetime.getSeconds());
+    var days = [
+      'воскресенье',
+      'понедельник',
+      'вторник',
+      'среда',
+      'четверг',
+      'пятница',
+      'суббота'
+    ];
+    var d = new Date();
+    var n = d.getDay();
+    var Ddays= days[n];
+    return Ddays+": "+day+"."+month+"."+year+" || время:"+hours+":"+minutes+":"+seconds;
+}
+
+/* выводим текущую дату и время на сайт в блок с id "current_date_time_block" */
+document.getElementById('date_clock').innerHTML = date_time();
+
 setInterval(function () {
-  var now = new Date();
-  var clock = document.getElementById("clock");
-  clock.innerHTML = now.toLocaleTimeString();
+  document.getElementById('date_clock').innerHTML = date_time();
 }, 1000);
