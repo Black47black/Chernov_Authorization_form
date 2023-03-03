490de8b43084a62474719e6d2d759054ad8f1a04
Dmitry Black <black47black@gmail.com>
2023-03-03 15:07:47 +0400

    add IE check

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//проверка валидности
 var form = document.getElementById("form");
 
 var phonenumberRegex = /^(\+7|8)(\()?\d{3}(\))?\d{7}$/g;
@@ -59,6 +59,7 @@ function zeroFirstFormat(value)
     return value;
 }
 
+//установка даты 
 function date_time()
 {
     var current_datetime = new Date();
@@ -83,8 +84,48 @@ function date_time()
     return Ddays+": "+day+"."+month+"."+year+" || время:"+hours+":"+minutes+":"+seconds;
 }
 
+//установка часов
 document.getElementById('date_clock').innerHTML = date_time();
 
 setInterval(function () {
   document.getElementById('date_clock').innerHTML = date_time();
 }, 1000);
+
+// function get_name_browser(){
+//   var sUsrAg  = navigator.userAgent;
+//   if (sUsrAg.indexOf("Trident") > -1)
+//    return "Microsoft Internet Explorer";
+//   if (sUsrAg.indexOf("Edge") > -1)
+//     return "Microsoft Edge";
+//   return 'Не определен';
+//   }
+var UsrAg = navigator.userAgent;
+
+//The order matters here, and this may report false positives for unlisted browsers.
+
+if (UsrAg.indexOf("Firefox") > -1) {
+     sBrowser = "Mozilla Firefox";
+     //"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+} else if (UsrAg.indexOf("Opera") > -1) {
+     sBrowser = "Opera";
+} else if (UsrAg.indexOf("Trident") > -1) {
+     sBrowser = "Microsoft Internet Explorer";
+     //"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; wbx 1.0.0; rv:11.0) like Gecko"
+} else if (UsrAg.indexOf("Edge") > -1) {
+     sBrowser = "Microsoft Edge";
+     //"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+} else if (UsrAg.indexOf("Chrome") > -1) {
+    sBrowser = "Google Chrome or Chromium";
+    //"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36"
+} else if (UsrAg.indexOf("Safari") > -1) {
+    sBrowser = "Apple Safari";
+    //"Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306"
+} else {
+    sBrowser = "unknown";
+}
+  
+if (sBrowser == 'Microsoft Edge' || sBrowser == 'Microsoft Internet Explorer') {
+    alert('Вы пользуетесь не тем браузером');
+} else {
+    alert('Браузер что надо!');
+}
